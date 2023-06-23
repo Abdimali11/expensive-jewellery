e8ba4ff84a6497770b3085e0ff7933193c33b8ae
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-23 11:28:35 -0700

    this is the answer of expensive-jewellery

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Good Luck 💪🏾
+
+  function findMostExpensiveJewelry(jewelryPieces) {
+    let maxPrice = 0;
+    let mostExpensiveJewelry = "";
+  
+    for (let piece in jewelryPieces) {
+      if (jewelryPieces.hasOwnProperty(piece)) {
+        const price = jewelryPieces[piece];
+  
+        if (price > maxPrice) {
+          maxPrice = price;
+          mostExpensiveJewelry = piece;
+        }
+      }
+    }
+  
+    return `The most expensive one is the ${mostExpensiveJewelry}.`;
+  }
+  const jewelryPieces = {
+    "Diamond Earrings": 980,
+    "Gold Watch": 250,
+    "Pearl Necklace": 4650
+  };
+
+  const result = findMostExpensiveJewelry(jewelryPieces);
+ 
+  console.log(result);
+ 
